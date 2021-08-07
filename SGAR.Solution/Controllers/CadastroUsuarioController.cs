ef58bb7b2f9f3b4752101726528e904f1528168a
Lucas Soares <lucasps6@hotmail.com>
2021-08-07 08:28:23 -0300

    Criado Classe Cadastro Usuario

---
+++ SGAR.Solution/Controllers/CadastroUsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGAR.WebApi.ViewModel.Pessoa;
+using System;
+
+namespace SGAR.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroUsuarioController : ControllerBase
+    {
+
+    }
+}
