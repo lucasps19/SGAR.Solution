b39e3e14af70793b6d00976ccd81677a879a3642
LucasPereira Soares <lucasps6@hotmail.com>
2021-11-14 19:26:27 -0300

    Criado metodo para retornar todos os dados de uma apreciacao de risco

--- SGAR.Dal/ListarApreciacoes/ListarApreciacoesDal.cs
+++ SGAR.Dal/ListarApreciacoes/ListarApreciacoesDal.cs
@@ -16,14 +16,30 @@ namespace SGAR.Dal.ListarApreciacoes
 
         public List<ApreciacaoRisco> BuscarApreciacoesRiscoDoUsuario(int idUsuario)
         {
-            var apreciacoes = _contexto.ApreciacoesRisco.Where(o => o.Pessoas.Contains(_contexto.Pessoas.Where(o => o.Id == idUsuario).FirstOrDefault())).ToList();
+            var apreciacoes = _contexto.ApreciacoesRisco
+                                       .Where(o => o.Pessoas.Contains(_contexto.Pessoas.Where(o => o.Id == idUsuario).FirstOrDefault()))
+                                       .Select(o => new { o.Id, o.Equipamento, o.DataApreciacao, o.LimiteEspaco, o.LimiteTempo, o.LimiteUso, o.Pessoas, o.IdEquipamento, o.Riscos})
+                                       .ToList();
+
+            List<ApreciacaoRisco> listaApreciacoesRisco = new List<ApreciacaoRisco>();
 
             foreach(var apr in apreciacoes)
             {
-                apr.Equipamento = _contexto.Equipamentos.Where(o => o.Id == apr.IdEquipamento).FirstOrDefault();
-            }            
+                listaApreciacoesRisco.Add(new ApreciacaoRisco
+                {
+                    Id = apr.Id,
+                    Equipamento = apr.Equipamento,
+                    LimiteEspaco = apr.LimiteEspaco,
+                    LimiteTempo = apr.LimiteTempo,
+                    LimiteUso = apr.LimiteUso,
+                    DataApreciacao = apr.DataApreciacao,
+                    IdEquipamento = apr.IdEquipamento,
+                    Riscos  = apr.Riscos,
+                    Pessoas = apr.Pessoas
+                });
+            }
 
-            return apreciacoes;
+            return listaApreciacoesRisco;
         }
     }
 }
